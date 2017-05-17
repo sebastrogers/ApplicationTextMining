51c7319a9207873fa2b58477c44dd3b053d6b3da
DESKTOP-P3G5S3N\sebas <sebast.rogers@gmail.com>
2017-05-17 16:13:57 -0300

    primeiro commit

---
+++ src/br/com/application/bagofwords/TextoBase.java
@@ -0,0 +1,56 @@
+package br.com.application.bagofwords;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class TextoBase {
+
+	public static void main(String[] args) throws IOException {
+		ArrayList<String> array = lerArquivo("PreconceitoConvertido.txt");
+		salvaArquivo(array);
+	}
+
+	static ArrayList<String> lerArquivo(String string) throws IOException {
+
+		System.out.println("Convertendo base...");
+		//FileReader arq = new FileReader(string);
+		BufferedReader lerArq = new BufferedReader(new InputStreamReader(new FileInputStream(string), "UTF-8"));
+		
+		ArrayList<String> indices = new ArrayList<String>();
+		String linha;
+		
+		/*
+		while ((linha = lerArq.readLine()) != null) {
+			indices.add(TransformaTexto.transformaTexto(linha));
+			System.out.println(linha);
+		}
+		*/
+		while((linha = lerArq.readLine()) != null){
+			//indices.add(StopWord.);
+		}
+		//arq.close();
+		return indices;
+
+	}
+
+	static void salvaArquivo(ArrayList<String> string) throws IOException {
+		FileWriter arqW = new FileWriter("PreconceitoConvertidoNOVO1.txt");
+		PrintWriter gravarArq = new PrintWriter(arqW);
+
+		for (String frase : string) {
+			gravarArq.println(frase);
+		}
+
+		arqW.close();
+		gravarArq.close();
+	}
+
+}
---
+++ src/br/com/application/bagofwords/TransformaTexto.java
@@ -0,0 +1,79 @@
+package br.com.application.bagofwords;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TransformaTexto {
+	
+	public static String transformaTexto(String texto){
+		
+		String transformado="";
+		Pattern p = Pattern.compile("[a-zA-Zà-ùÀ-Ùá-úÁ-Ú0-9]+");
+	    Matcher m = p.matcher(texto);
+	    while(m.find()) {
+	    	transformado+= m.group()
+	    	.replace("à", "a")
+	    	.replace("è", "e")
+	    	.replace("ì", "i")
+	    	.replace("ò", "o")
+	    	.replace("ù", "u")
+	    	.replace("á", "a")
+	    	.replace("é", "e")
+	    	.replace("í", "i")
+	    	.replace("ó", "o")
+	    	.replace("ú", "u")
+	    	.replace("ã", "a")
+	    	.replace("õ", "o")
+	    	.replace("â", "a")
+	    	.replace("ê", "e")
+	    	.replace("î", "i")
+	    	.replace("ô", "o")
+	    	.replace("û", "u")
+	    	.replace("ç", "c")
+	    	+" ";
+	    	
+	    }
+	    
+		return transformado;
+	}
+	
+	public static String stopwords(String texto){
+		
+		String transformado="";
+		Pattern p = Pattern.compile("[a-zA-Zà-ùÀ-Ùá-úÁ-Ú0-9]+");
+	    Matcher m = p.matcher(texto);
+	    while(m.find()) {
+	    	transformado+= m.group()
+	    	.replace("de", "")
+	    	.replace("a", "")
+	    	.replace("o", "")
+	    	.replace("e", "")
+	    	.replace("do", "")
+	    	.replace("da", "")
+	    	.replace("em", "")
+	    	.replace("um", "")
+	    	.replace("para", "")
+	    	+" ";
+	    	
+	    	 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			
+	    	
+	    }
+	    
+		return transformado;
+	}
+
+	public static void main(String[] args) throws IOException {
+	
+		
+	}
+
+}
